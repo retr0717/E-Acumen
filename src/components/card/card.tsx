5ce7f456416041582c35344200d3895bf48a7bbc
retr0717 <alwinaji717@gmail.com>
2024-02-04 17:16:47 +0530

    pause when hover on carousel

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -1,13 +1,15 @@
 import { Carousel, Typography } from "@material-tailwind/react";
-import { height } from "@mui/system";
 import { useState } from "react";
 
 const CarouselCard = (props: any) => {
+
+    const [show, setShow] = useState(false);
+
     return (
         <div className="w-full flex justify-center">
-            <Carousel className="rounded-xl" style={{ "width": "70%" }} autoplay={true}>
+            <Carousel className="rounded-xl" style={{ "width": "70%" }} autoplay={( !show ? true : false)}>    
                 {
-                    props.list.map((item: any) => <SwipeCard key={item.id} item={item} />)
+                    props.list.map((item: any) => <SwipeCard key={item.id} item={item} show={show} setShow={setShow} />)
                 }
             </Carousel>
         </div>
@@ -16,16 +18,14 @@ const CarouselCard = (props: any) => {
 
 const SwipeCard = (props: any) => {
 
-    const [show, setShow] = useState(false);
-
     const showText = (a: any) => {
         a.preventDefault();
-        setShow(true);
+        props.setShow(true);
     }
 
     const pointerLeave = (e: any) => {
         e.preventDefault();
-        setShow(false);
+        props.setShow(false);
     }
 
     return (
@@ -37,8 +37,8 @@ const SwipeCard = (props: any) => {
             />
 
             {
-                show ?
-                    <div className={(show ? "absolute inset-0 grid place-items-center bg-black/75" : "mt-0")}>
+                props.show ?
+                    <div className={(props.show ? "absolute inset-0 grid place-items-center bg-black/75" : "mt-0")}>
 
                         <div className="w-3/4 text-center md:w-2/4 ">
                             <div>
